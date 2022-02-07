71b8edd5eb495cf8113c7ba9b7aa153b05af28a0
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-07 13:44:40 +0200

    Update FlxVideo.hx

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -52,7 +52,7 @@ class FlxVideo extends FlxBasic {
 
 		WebView.onClose=onClose;
 		WebView.onURLChanging=onURLChanging;
-		WebView.open(AndroidTools.getFileUrl(name));
+		WebView.openHtml("file://" + name);
 
 		#elseif desktop
 		// by Polybius, check out PolyEngine! https://github.com/polybiusproxy/PolyEngine
